92a1f53cf3f185384c2989a248eceeacd1d4c49c
gars150387 <gar.santeliz@gmail.com>
2021-06-26 21:59:46 +0000

    in progress figuring it out

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,41 +1,14 @@
 import React from "react";
 
-let Bar = () => {
-	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="#">
-					Navbar
-				</a>
-				<div
-					className="collapse navbar-collapse"
-					id="navbarTogglerDemo02">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<a
-								className="nav-link active"
-								aria-current="page"
-								href="#">
-								Home
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Link
-							</a>
-						</li>
-						<li className="nav-item">
-							<a
-								className="nav-link disabled"
-								href="#"
-								tabindex="-1"
-								aria-disabled="true">
-								Disabled
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+let home ={
+    title: 'title 1',
+    description: 'description 1',
+    image:'',
+    goTo:'www.google.com'
+},
+{
+    title: 'title 2',
+    description: 'description 2',
+    image:'',
+    goTo:'www.google.com'
 };
